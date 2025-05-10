ccfcf78eb5e5d8b4852f62f3803427c3e0159df9
SOCR-470 <rssmoris@gmail.com>
2025-05-10 14:59:18 -0300

    Atualiza app para exibir logo no topo

--- app5.py
+++ app5.py
@@ -175,20 +175,15 @@ def registrar_agendamento(nome, detalhes, horario, sucesso):
         st.error(f"Erro ao salvar agendamento: {str(e)}")
 
 # Interface
-st.set_page_config(page_title="JusIA - Atendimento Jurídico", page_icon="logo_pmp.png")  # se for local
-
-# Para exibir a logo no topo
-st.image("logo_pmp.png", width=300)  # se local
-
-# Ou se quiser usar o link direto:
-# st.image("https://www.pmp.com.br/logo.png", width=300)
-
-st.title("JusIA - Assistente Jurídico Virtual")
+st.set_page_config(page_title="JusIA - Atendimento Jurídico", page_icon="logo_pmp.png")
 
 st.markdown(
     "<div style='text-align: center'><img src='logo_pmp.png' width='300'/></div>",
     unsafe_allow_html=True
 )
+
+st.title("JusIA - Assistente Jurídico Virtual")
+
 
 
 for msg in st.session_state.historico_chat[1:]:
